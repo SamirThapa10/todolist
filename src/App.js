f34737962a1c33d04a9c90b2b8358cd71589240c
Samir Thapa <sameerthapa159gmail.com>
2025-01-03 21:10:14 -0600

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Show from "./components/Show";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Show />
+      </header>
+    </div>
+  );
+}
+
+export default App;
